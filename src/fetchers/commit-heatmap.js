46c2ebd6341446e5c50b318d20fdc00edf6b2d26
Siddhartha Roy <89271421+roysiddharth@users.noreply.github.com>
2026-05-08 10:58:09 +0530

    return date range from commit heatmap fetcher

--- src/fetchers/commit-heatmap.js
+++ src/fetchers/commit-heatmap.js
@@ -7,7 +7,7 @@ import { MissingParamError } from "../common/error.js";
  * day: 0=Mon ... 6=Sun, hour: 0-23 UTC
  *
  * @param {string} username GitHub username.
- * @returns {Promise<{ matrix: number[][], total: number }>}
+ * @returns {Promise<{ matrix: number[][], total: number, from: Date, to: Date }>}
  */
 const fetchCommitHeatmap = async (username) => {
   if (!username) throw new MissingParamError(["username"]);
@@ -24,14 +24,22 @@ const fetchCommitHeatmap = async (username) => {
   );
 
   const matrix = Array.from({ length: 7 }, () => new Array(24).fill(0));
+  const dates = [];
+
   for (const item of res.data.items) {
     const date = new Date(item.commit.author.date);
+    dates.push(date);
     const day = (date.getUTCDay() + 6) % 7; // 0=Mon, 6=Sun
     const hour = date.getUTCHours();
     matrix[day][hour]++;
   }
 
-  return { matrix, total: res.data.items.length };
+  return {
+    matrix,
+    total: res.data.items.length,
+    from: dates[dates.length - 1] ?? new Date(),
+    to: dates[0] ?? new Date(),
+  };
 };
 
 export { fetchCommitHeatmap };

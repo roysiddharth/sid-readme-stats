0ba2817301527188b507c2d112485d149ff9d1b0
Siddhartha Roy <89271421+roysiddharth@users.noreply.github.com>
2026-05-08 10:54:55 +0530

    add commit heatmap API endpoint

---
+++ api/commit-heatmap.js
@@ -0,0 +1,93 @@
+// @ts-check
+import { renderCommitHeatmap } from "../src/cards/commit-heatmap.js";
+import { guardAccess } from "../src/common/access.js";
+import {
+  CACHE_TTL,
+  resolveCacheSeconds,
+  setCacheHeaders,
+  setErrorCacheHeaders,
+} from "../src/common/cache.js";
+import {
+  MissingParamError,
+  retrieveSecondaryMessage,
+} from "../src/common/error.js";
+import { parseBoolean } from "../src/common/ops.js";
+import { renderError } from "../src/common/render.js";
+import { fetchCommitHeatmap } from "../src/fetchers/commit-heatmap.js";
+
+// @ts-ignore
+export default async (req, res) => {
+  const {
+    username,
+    title_color,
+    text_color,
+    bg_color,
+    border_color,
+    theme,
+    hide_border,
+    hide_title,
+    border_radius,
+    cache_seconds,
+    custom_title,
+    disable_animations,
+  } = req.query;
+
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  const access = guardAccess({
+    res,
+    id: username,
+    type: "username",
+    colors: { title_color, text_color, bg_color, border_color, theme },
+  });
+  if (!access.isPassed) return access.result;
+
+  try {
+    const data = await fetchCommitHeatmap(username);
+    const cacheSeconds = resolveCacheSeconds({
+      requested: parseInt(cache_seconds, 10),
+      def: CACHE_TTL.STATS_CARD.DEFAULT,
+      min: CACHE_TTL.STATS_CARD.MIN,
+      max: CACHE_TTL.STATS_CARD.MAX,
+    });
+    setCacheHeaders(res, cacheSeconds);
+    return res.send(
+      renderCommitHeatmap(data, {
+        title_color,
+        text_color,
+        bg_color,
+        border_color,
+        theme,
+        hide_border: parseBoolean(hide_border),
+        hide_title: parseBoolean(hide_title),
+        border_radius,
+        custom_title,
+        disable_animations: parseBoolean(disable_animations),
+      }),
+    );
+  } catch (err) {
+    setErrorCacheHeaders(res);
+    if (err instanceof Error) {
+      return res.send(
+        renderError({
+          message: err.message,
+          secondaryMessage: retrieveSecondaryMessage(err),
+          renderOptions: {
+            title_color,
+            text_color,
+            bg_color,
+            border_color,
+            theme,
+            show_repo_link: !(err instanceof MissingParamError),
+          },
+        }),
+      );
+    }
+    return res.send(
+      renderError({
+        message: "An unknown error occurred",
+        renderOptions: { title_color, text_color, bg_color, border_color, theme },
+      }),
+    );
+  }
+};
